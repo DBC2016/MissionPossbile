8c7f2048b25e45f82d02f930db16aeb9df58a8f5
Demond Childers <Demond@demonds-macbook-pro.local>
2016-04-11 13:47:32 -0400

    Initial Commit

---
+++ Mission Possible/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Mission Possible
+//
+//  Created by Demond Childers on 4/11/16.
+//  Copyright © 2016 Demond Childers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
